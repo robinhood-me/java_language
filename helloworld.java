c8c2fab9740265c4faed263e679fe204ea3ffabf
Robin Hood <rhcrco@gmail.com>
2019-06-22 18:32:51 -0700

    // added maven to build the code. let's see if by a miracle of no maven configuration, if bamboo can actually build this.

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
 //Try to get webhood to trigger a build when there is a change in code
 //Try two
 // turned of ssl on webhood on bigbucket side to see if webhood will quit having the Missing XSRF token
+// added maven to build the code. let's see if by a miracle of no maven configuration, if bamboo can actually build this.
 
 public class HelloWorld {
 
