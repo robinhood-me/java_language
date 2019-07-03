cc24f9074e1d4f46c44e4c0ea9b32a3da0503c1e
Robin Hood <rhcrco@gmail.com>
2019-07-03 13:58:13 -0700

    change for test push to trigger jenkins from github webhook

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,8 @@
 // turned of ssl on webhood on bigbucket side to see if webhood will quit having the Missing XSRF token
 // added maven to build the code. let's see if by a miracle of no maven configuration, if bamboo can actually build this.
 
+// push to github for jenkins trigger build test
+
 public class HelloWorld {
 
     public static void main(String[] args) {
